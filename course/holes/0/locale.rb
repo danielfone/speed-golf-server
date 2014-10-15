e1103af7e850c728805adfd65c484191ab484d37
Daniel Fone <daniel@fone.net.nz>
2014-10-15 22:20:24 +1300

    Locale

--- course/holes/0/locale.rb
+++ course/holes/0/locale.rb
@@ -1,41 +1,44 @@
 require_relative '../../lib/micro_bench'
 require_relative 'country_codes'
 
+# This function returns the name of a country for an ISO3166-1 alpha-3 code.
+# If you pass a valid country name, it will just return that instead.
+# The look up is case-insensitive.
+
 module Locale
   module_function
 
   def country_name(lookup)
-    COUNTRY_CODE_MAP[lookup.upcase] or COUNTRY_CODE_MAP.values.find {|c|c.casecmp(lookup) == 0}
-  end
-
-  def slower0(lookup)
-    lookup = lookup.upcase
-    COUNTRY_CODE_MAP[lookup] or COUNTRY_CODE_MAP.each_value.find do |country|
-      country.upcase == lookup
-    end
-  end
-
-  def slower(lookup)
-    COUNTRY_CODE_MAP.each do |code,country|
-      return country if [code.downcase, country.downcase].include? lookup.downcase
-    end
-    nil
-  end
-
-  def slower2(lookup)
     matches = COUNTRY_CODE_MAP.select do |code, country|
       [code.downcase, country.downcase].include? lookup.downcase
     end
     matches.first.last if matches.any?
   end
 
+  def lazy_country_name(lookup)
+    match = COUNTRY_CODE_MAP.find do |code, country|
+      [code.downcase, country.downcase].include? lookup.downcase
+    end
+    match.last if match
+  end
+
+  def keyed_country_name(lookup)
+    lookup = lookup.upcase
+    COUNTRY_CODE_MAP[lookup] or COUNTRY_CODE_MAP.each_value.find do |v|
+      v.upcase == lookup
+    end
+  end
+
+  def casecmp_country_name(lookup)
+    COUNTRY_CODE_MAP[lookup.upcase] or COUNTRY_CODE_MAP.values.find do |v|
+      v.casecmp(lookup) == 0
+    end
+  end
 
 end
 
-100.times { Locale.slower0 'denmark' }
-100.times { Locale.country_name 'denmark' }
-
-exit
+100.times { Locale.keyed_country_name 'denmark' }
+#exit
 
 b = MicroBench.new Locale, 'denmark', {
   'NZL'         => 'New Zealand',
@@ -45,4 +48,4 @@ b = MicroBench.new Locale, 'denmark', {
   'foo'         => nil,
 }
 
-b.check :country_name, :slower, :slower2, :slower0
+b.check :country_name, :lazy_country_name, :keyed_country_name, :casecmp_country_name
