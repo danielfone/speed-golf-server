6e4310855a5837094974a30d7afd2f0cff3bc26b
Daniel Fone <daniel@fone.net.nz>
2014-10-16 15:02:14 +1300

    Sample

--- course/holes/5/sample.rb
+++ course/holes/5/sample.rb
@@ -1,5 +1,3 @@
-require_relative '../../lib/micro_bench'
-
 # Randomly return half the numbers under the provided limit
 # e.g.
 #   sample_half(10) => [1,3,4,5,8]
@@ -20,26 +18,12 @@ require_relative '../../lib/micro_bench'
 #
 # ~30x improvement available
 
-require 'set'
+require_relative '../micro_bench'
 
 module Sample
   module_function
 
   def sample_half(limit)
-    (1..limit).to_a.sample(limit/2)
-  end
-
-  def array(limit)
-    count = limit/2
-    sample = []
-    while sample.count < count
-      n = rand limit
-      sample << n unless sample.include? n
-    end
-    sample
-  end
-
-  def del(limit)
     count = limit/2
     population = (1..limit).to_a
     sample = []
@@ -50,70 +34,14 @@ module Sample
     sample
   end
 
-  def upto(limit)
-    count = limit/2
-    jump = 2
-    sample = {}
-    i = 0
-    count.times do
-      i += @prng.rand(jump)+1
-      sample[i % limit] = true
-    end
-    sample.keys
-  end
-
-  def hash(limit)
-    count = limit/2
-    tmp = nil
-    x = {}
-
-    for i in 0...count
-        while x.has_key?(tmp = rand(limit))
-        end
-        x[tmp] = true
-    end
-
-    x.keys
-  end
-
-  @prng = Random.new
-  def hash2(limit)
-    count = limit/2
-    sample = {}
-
-    while sample.size < count
-      sample[@prng.rand(limit)] = true
-    end
-    sample.keys
-  end
-
-
-  def set(limit)
-    count = limit/2
-    sample = Set.new
-    prng = Random.new
-    while sample.size < count
-      sample.add prng.rand(limit)
-    end
-    sample.to_a
-  end
-
 end
 
 TEST_METHODS = [
-#  :sample_half,
-  :del,
-  :hash,
-  :hash2,
-  :set,
-  :upto,
+  :sample_half,
+  # Add your methods in here...
 ]
 
-#Sample.hash2 10000
-#Sample.upto 100000
-##Sample.set 100000
-#exit
-
+# Unconvential tests because the output is random
 TEST_METHODS.each do |m|
   limit = 100
   count = limit / 2
