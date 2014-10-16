e35d8c9700caed7f8feb604077db91b4d3aa2d29
Daniel Fone <daniel@fone.net.nz>
2014-10-16 14:58:17 +1300

    Fibonacci

--- course/holes/3/fibonacci.rb
+++ course/holes/3/fibonacci.rb
@@ -1,41 +1,27 @@
-require_relative '../../lib/micro_bench'
-
 # Return the nth fibonacci number
 #
 # ~300x improvement available
 #
 # Bonus points: increase n and see if different algorithms perform better
 
+require_relative '../micro_bench'
+
 module Fibonacci
   module_function
 
-  def recursive(n)
-    n <= 1 ? n : recursive(n-1) + recursive(n-2)
+  def fibonacci(n)
+    n <= 1 ? n : fibonacci(n-1) + fibonacci(n-2)
   end
-
-  @fib = [0, 1, 1]
-  def memoized(n)
-    @fib[n] ||= memoized(n-2) + memoized(n-1)
-  end
-
-  def iter(n)
-    curr_num, next_num = 0, 1
-    (n).times do
-      curr_num, next_num = next_num, curr_num + next_num
-    end
-    curr_num
-  end
-
 
 end
 
 b = MicroBench.new Fibonacci, 15, {
-  1 => 1,
-  2 => 1,
-  3 => 2,
-  4 => 3,
+  1  => 1,
+  2  => 1,
+  3  => 2,
+  4  => 3,
   10 => 55,
   15 => 610,
   20 => 6765,
 }
-b.check :recursive, :memoized, :iter
+b.check :fibonacci
